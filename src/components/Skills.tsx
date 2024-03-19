ba1d7db1ed4f421991abbb0e367d2bff92f5f4cf
nick-vi <nicolas.vilvovsky@gmail.com>
2024-03-19 15:20:09 +0200

    Refactor Skills component to support categorized skills

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -6,18 +6,34 @@ export default function Experience() {
 
   if (skills.length === 0) return null;
 
+  const categorizedSkills = typeof skills[0] === 'object';
+
   return (
     <Section title="Skills">
-      <div className="flex flex-wrap gap-1">
-        {skills.map((skill) => (
-          <span
-            key={skill}
-            className="bg-gray-100 text-gray-800 px-2 py-0.5 rounded-md text-2xs border border-gray-300"
-          >
-            {skill}
-          </span>
-        ))}
-      </div>
+      {categorizedSkills
+        ? skills.map(({ name, items }) => (
+            <>
+              <h2 className="block font-semibold">{name}</h2>
+              <div className="flex flex-wrap gap-1">
+                {items.map((item) => (
+                  <span
+                    key={item}
+                    className="bg-gray-100 text-gray-800 px-2 py-0.5 rounded-md text-2xs border border-gray-300"
+                  >
+                    {item}
+                  </span>
+                ))}
+              </div>
+            </>
+          ))
+        : skills.map((skill) => (
+            <span
+              key={skill}
+              className="bg-gray-100 text-gray-800 px-2 py-0.5 rounded-md text-2xs border border-gray-300"
+            >
+              {skill}
+            </span>
+          ))}
     </Section>
   );
 }
